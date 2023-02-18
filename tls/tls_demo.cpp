c3ac12bd1cdc65e4c8aa7b6383e81a4a7d62387b
CodeStarr <starr_x@163.com>
2023-02-18 09:54:13 +0800

    demo update

--- tls/tls_demo.cpp
+++ tls/tls_demo.cpp
@@ -1,16 +1,41 @@
 #include "tls_demo.h"
 
-void client()
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void client(unsigned short port, char* ip)
 {
 
 }
 
-void server()
+void server(unsigned short port)
 {
 
 }
 
 void test_tls(int argc, char** argv)
 {
+    unsigned short port = 20300;
+    if (argc > 1)
+    {
+        
+        port = atoi(argv[1]);
+    }
 
+    do {
+
+        if (argc > 2)
+        {
+            // client
+            // EXE PORT IP
+            client(port, argv[2]);
+            break;
+        }
+        server(port);
+    }while(0);
+
+    getchar();
+    return;
 }
